c5f8feaaaa2dcebb242fd153429fc48073fabb1f
guitarbien <guitarbien@gmail.com>
2017-08-03 21:39:26 +0800

    TestCase：在 input 輸入'Study Angular'按下 enter 後會將 input 清空
    要有 setTodo() 可以設定 input 的內容，並模擬按下 enter
    之後 assert getTodo() 應要為 ‘’
    
    紅燈

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 
 import { DemoPage } from './app.po';
+import { protractor } from 'protractor/built/ptor';
 
 describe('demo App', () => {
   let page: DemoPage;
@@ -16,5 +17,13 @@ describe('demo App', () => {
   it(`should display 'What needs to be done?' on 'placeholder' attribute`, () => {
     page.navigateTo();
     expect(page.getPlaceHolder()).toBe('What needs to be done?');
+  });
+
+  it(`在 input 輸入'Study Angular'按下 enter 後會將 input 清空`, () => {
+    page.navigateTo()
+      .setTodo('Study Angular')
+      .setTodo(protractor.Key.ENTER);
+
+    expect(page.getTodo().toBe(''));
   });
 });
