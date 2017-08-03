e7704d6f2a6ad6a595da63aee50ca25a3378a97d
guitarbien <guitarbien@gmail.com>
2017-08-03 22:28:21 +0800

    page object 加上對應測試的 setTodo() getTodo() 並對先前的 method 補上回傳型態

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,15 +1,25 @@
 import { browser, by, element } from 'protractor';
 
 export class DemoPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(): DemoPage {
+    browser.get('/');
+    return this;
   }
 
-  getTodos() {
+  getTodos(): any {
     return element(by.css('app-root h1')).getText();
   }
 
-  getPlaceHolder() {
+  getPlaceHolder(): any {
     return element(by.css('.new-todo')).getAttribute('placeholder');
+  }
+
+  setTodo(input: string): DemoPage {
+    element(by.css('.new-todo')).sendKeys(input);
+    return this;
+  }
+
+  getTodo(): any {
+    return element(by.css('.new-todo')).getAttribute('value');
   }
 }
