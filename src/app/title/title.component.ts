267046496625450865fbd0518f64efc5a6f5e77d
guitarbien <guitarbien@gmail.com>
2017-08-03 21:26:39 +0800

    getTitle() 改為使用 arrow function

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -8,9 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TitleComponent implements OnInit {
   title: string;
 
-  getTitle = () => {
-    return this.title;
-  }
+  getTitle = () => this.title;
 
   constructor() { }
 
